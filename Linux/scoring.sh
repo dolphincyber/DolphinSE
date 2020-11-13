07f28b51a1e1031731f83b6ed0abeb84df413fa6
Dolphin <57072181+dolphincyber@users.noreply.github.com>
2020-11-12 18:52:22 -0800

    prepared for windows SE

--- Linux/scoring.sh
+++ Linux/scoring.sh
@@ -10,15 +10,15 @@ function checks {
     echo $vulns > ./assets/previous.txt
 }
 
-# function alert {
-#     if [ "$vulns" -gt "$prevVulns" ]; then
-#         rhythmbox-client --play ./assets/mp3/gain.mp3
-#         notify-send 'DolphinSE' 'You gained points!'
-#     elif [ "$vulns" -lt "$prevVulns" ]; then
-#         rhythmbox-client --play ./assets/mp3/warn.mp3
-#         notify-send 'DolphinSE' 'You lost points!'
-#     fi
-# }
+function alert {
+    if [ "$vulns" -gt "$prevVulns" ]; then
+        play -q ./assets/mp3/gain.mp3
+        notify-send -i "./assets/img/icon.ico" "You gained points!"
+    elif [ "$vulns" -lt "$prevVulns" ]; then
+        rhythmbox-client --play ./assets/mp3/warn.mp3
+        play -q "./assets/img/icon.ico" "You lost points!"
+    fi
+}
 
 checks
 #alert
